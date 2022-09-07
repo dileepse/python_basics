63732b3ab34396bd853580b63b45bd3ed06b8343
dileepse <dasari.dileepkumar@gmail.com>
2022-09-07 08:24:42 +0530

    codes

--- Loops_20220907/loops_4.py
+++ Loops_20220907/loops_4.py
@@ -1,7 +1,7 @@
 # WAP to find average of given elements in the list
 # sum and count 
 
-def get_list_count(inp_lst): # inp_lst = lst1
+def get_list_count(inp_lst): # inp_lst = lst
     """
     Description : This function is to find the length of list or string
     arguments :
@@ -14,7 +14,7 @@ def get_list_count(inp_lst): # inp_lst = lst1
         count = count + 1
     return count
 
-def get_list_sum(some_list):
+def get_list_sum(some_list): # some_list = lst
     """
     Description : This function is to find the sum of list elements
     arguments :
@@ -27,16 +27,17 @@ def get_list_sum(some_list):
         result_sum = result_sum + ele
     return result_sum
         
-def get_average(pass_sum,  pass_cnt):
-    return pass_sum/pass_cnt
+def get_average(pass_sum,  pass_cnt): # pass_sum = 210, pass_cnt = 6
+    return pass_sum/pass_cnt # 210/6
 
 lst = [10,20,30,40,50,60] 
+
 count_res = get_list_count(lst)
 
-sum_res = get_list_sum(lst)
+sum_res = get_list_sum(lst) # 210
 
-
-result_average = get_average(sum_res,  count_res)
+result_average = get_average(sum_res, count_res) 
+# assignment operation(3 parts LHS, RHS,assignment)/function calling
 
 print(result_average)
 
